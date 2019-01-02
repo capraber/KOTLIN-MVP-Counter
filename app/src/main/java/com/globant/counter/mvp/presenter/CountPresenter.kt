b6e4afa225317b2aba5fd426597b5e5d12481dd1
GLOBANT\taidy.marrugo <taidy.marrugo@globant.com>
2019-01-02 17:29:13 -0500

    count presenter updated

--- app/src/main/java/com/globant/counter/mvp/presenter/CountPresenter.kt
+++ app/src/main/java/com/globant/counter/mvp/presenter/CountPresenter.kt
@@ -3,28 +3,46 @@ package com.globant.counter.mvp.presenter
 import com.globant.counter.mvp.model.CalculatorModel
 import com.globant.counter.mvp.view.CountView
 import com.globant.counter.utils.bus.RxBus
+import com.globant.counter.utils.bus.observer.OnCalculatorActionItemPressedBusObserver
+import com.globant.counter.utils.bus.observer.OnCalculatorClearButtonPressedBusObserver
+import com.globant.counter.utils.bus.observer.OnCalculatorEqualButtonPressedBusObserver
+import com.globant.counter.utils.bus.observer.OnCalculatorNumberButtonPressedBusObserver
 
-class CountPresenter(model: CalculatorModel, view: CountView) {
+open class CountPresenter(model: CalculatorModel, view: CountView) {
 
     init {
 
         val activity = view.activity
         if (activity != null) {
-
-            RxBus.subscribe(activity, object : OnCountButtonPressedBusObserver() {
-                override fun onEvent(value: OnCountButtonPressedBusObserver.OnCountButtonPressed) {
-                    model.inc()
-                    view.setCount(model.count.toString())
+            RxBus.subscribe(activity, object : OnCalculatorActionItemPressedBusObserver() {
+                override fun onEvent(value: OnCalculatorActionButtonPressed) {
+                    model.action = value.action
+                    view.setExpression(model.getExpression())
                 }
             })
 
-            RxBus.subscribe(activity, object : OnResetButtonPressedBusObserver() {
-                override fun onEvent(value: OnResetButtonPressedBusObserver.OnResetButtonPressed) {
-                    model.reset()
-                    view.setCount(model.count.toString())
+            RxBus.subscribe(activity, object : OnCalculatorNumberButtonPressedBusObserver() {
+                override fun onEvent(value: OnCalculatorNumberButtonPressed) {
+                    model.setOperator(value.number)
+                    view.setExpression(model.getExpression())
                 }
             })
+
+            RxBus.subscribe(activity, object  : OnCalculatorEqualButtonPressedBusObserver() {
+                override fun onEvent(value: OnCalculatorEqualButtonPressed) {
+                    view.setExpression(model.getResult())
+                }
+            })
+
+            RxBus.subscribe(activity, object : OnCalculatorClearButtonPressedBusObserver() {
+                override fun onEvent(value: OnCalculatorClearButton) {
+                    model.clear()
+                    view.clear()
+                }
+
+            })
+
         }
-
     }
+
 }
