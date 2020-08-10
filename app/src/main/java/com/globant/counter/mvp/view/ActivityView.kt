fd8ca9046cd9f3b04cc2edfa24cc44d02e4b6edb
benjaglobant <61156769+benjaglobant@users.noreply.github.com>
2020-08-10 16:57:32 -0300

    MVP and Tests Refactor (#8)
    * MVP and Tests Refactor
    
    * README refactored
    
    Co-authored-by: benjaglobant <benjaglobant@users.noreply.github.com>

--- app/src/main/java/com/globant/counter/mvp/view/ActivityView.kt
+++ app/src/main/java/com/globant/counter/mvp/view/ActivityView.kt
@@ -1,9 +1,6 @@
 package com.globant.counter.mvp.view
 
 import android.app.Activity
-import android.app.FragmentManager
-import android.content.Context
-
 import java.lang.ref.WeakReference
 
 open class ActivityView(activity: Activity) {
@@ -11,13 +8,4 @@ open class ActivityView(activity: Activity) {
 
     val activity: Activity?
         get() = activityRef.get()
-
-    val context: Context?
-        get() = activity
-
-    val fragmentManager: FragmentManager?
-        get() {
-            val activity = activity
-            return activity?.fragmentManager
-        }
 }
--- app/src/main/java/com/globant/counter/rx/Events.kt
+++
@@ -1,6 +0,0 @@
-package com.globant.counter.rx
-
-object EventTypes {
-    const val INCREMENT_EVENT = 0
-    const val RESET_COUNT_EVENT = 1
-}
